73976ea491b69a286f1f4be980b38bbadb9f4a8d
taflynn <thomas.a.flynn@durham.ac.uk>
2025-08-13 11:02:15 +0100

    added the intra-node table which displays intra-node metrics

--- tools/intra_node.py
+++ tools/intra_node.py
@@ -30,14 +30,9 @@ class intra_node_perf():
         self.p_critical_60 = number_of_cores[i-1]
         self.intraNode_proportion_60 = self.p_critical_60 / max(number_of_cores)
 
-        print(r'80% intra-node metric:', self.intraNode_proportion_80)
-        print(r'60% intra-node metric:', self.intraNode_proportion_60)
-
     def plot_efficiency_graph(self):
         ''' Plot Efficiency against Number of Cores graph '''
         fig, ax = plt.subplots()
-        #ax.axhline(y=0.8, color="#ffc844", linestyle="--")
-        #ax.axhline(y=0.6, color="#e35555", linestyle="--")
         ax.axvline(x=self.p_critical_80, color="#ffc844", linestyle="--")
         ax.axvline(x=self.p_critical_60, color="#e35555", linestyle="--")        
         ax.set_title('Efficiency against Number of Cores')
@@ -56,4 +51,38 @@ class intra_node_perf():
         ax.set_ylabel("Time")
         ax.set_xticks(self.number_of_cores)
         plt.plot(self.number_of_cores, self.time, '.-', color = 'black', linewidth=2)
+        plt.show()
+
+    def intra_perf_table(self):
+        ''' Draw a table indicating flaws in code '''
+        data = [[''], [''], ['']]
+        column_headings = [r'Efficiency']
+        row_headings = [r'$C_{\mathrm{intra}}^{80\%} \geq 0.8$',r'$C_{\mathrm{intra}}^{80\%} < 0.8 \wedge C_{\mathrm{intra}}^{60\%} \geq 0.6$',r'otherwise']
+
+        perf_str = [r'$C_{\mathrm{intra}}^{80\%} =$' + f'{self.intraNode_proportion_80:.2f}' + r', $C_{\mathrm{intra}}^{60\%}$' + f'{self.intraNode_proportion_60:.2f}']
+        
+        if self.intraNode_proportion_80 >= 0.8:
+            data[0] = perf_str
+        elif self.intraNode_proportion_80 < 0.8 and self.intraNode_proportion_60 >= 0.6:
+            data[1] = perf_str
+        else:
+            data[2] = perf_str
+        
+        fig, ax = plt.subplots()
+        ax.set_axis_off()
+        
+        cell_colours = [['#0fa834'], ['#ffc844'], ['#e35555']]
+
+        plt.rcParams.update({'font.size': 18}) 
+
+        table = plt.table(cellText = data,
+                          rowLabels = row_headings,
+                          colLabels = column_headings,
+                          loc = 'center',
+                          colLoc = 'center',
+                          cellLoc = 'center',
+                          rowLoc = 'center',
+                          cellColours = cell_colours)
+
+        table.scale(1.5,3)
         plt.show()
