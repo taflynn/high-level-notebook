4ea8235b98ac3a87ac3b7224f6c903461d1ade44
taflynn <thomas.a.flynn@durham.ac.uk>
2025-10-31 11:24:48 +0000

    updating the GPU performance metric interfae to be 1D

--- topics/gpu_table.py
+++ topics/gpu_table.py
@@ -1,35 +1,35 @@
 import matplotlib.pyplot as plt
 class gpu_perf:
-    def __init__(self, gpu_utilisation, measured_memory, peak_memory):
-        ''' Calculate statistics about Core Performance '''
+    def __init__(self, gpu_util):
+        '''Raw GPU performance data'''
         
-        self.gpu_utilisation = gpu_utilisation
-        self.measured_memory = measured_memory
-        self.peak_memory = peak_memory
-        
-        self.memory_usage = self.measured_memory/self.peak_memory
+        self.gpu_util = gpu_util
+
+        self.gpu_metric = self.gpu_util
    
     def gpu_perf_table(self):
-        ''' Draw a table indicating flaws in code '''
+        '''Generates a traffic-light table for the GPU performance metric'''
         
-        data = [['','',''],['','',''],['','','']]
-        column_headings = [r'$C_{\mathrm{mem}} \geq 0.8$',r'0.8 > $C_{\mathrm{mem}} \geq 0.6$',r'$0.6 > C_{\mathrm{mem}}$']
-        row_headings = [r'$C_{\mathrm{util}} \geq 0.8$',r'0.8 > $C_{\mathrm{util}} \geq 0.6$',r'$0.6 > C_{\mathrm{util}}$']
+        data = [[''],[''],['']]
+        column_headings = [r'GPU performance metric']
+        row_headings = [r'$C_{\mathrm{util}} \geq 0.8$', 
+                        r'0.8 > $C_{\mathrm{util}} \geq 0.6$', 
+                        r'$0.6 > C_{\mathrm{util}}$']
+
+        perf_str = [r'$C_{\mathrm{util}} \approx$' + f'{self.gpu_util:.3f}']
+        
+        if self.gpu_util >= 0.8:
+            data[0] = perf_str
+        elif self.gpu_util >= 0.6:
+            data[1] = perf_str
+        else:
+            data[2] = perf_str
         
         fig, ax = plt.subplots()
         ax.set_axis_off()
         
-        cell_colours = [['#0fa83480','#0fa83480','#ffc84480'],
-                        ['#0fa83480','#ffc84480','#ffc84480'],
-                        ['#e3555580','#e3555580','#e3555580']]
-        
-        # find cell for statistics
-        A,B = (self.gpu_utilisation >= 0.8),(self.gpu_utilisation >= 0.6)
-        C,D = (self.memory_usage >= 0.8),(self.memory_usage >= 0.6)
-        option = (A and C)*0 + (A and D and not(C))*1 + (A and not(D))*2 + (C and not(A) and B)*3 + (not(A or C) and B and D)*4
-        option += (not(A or D) and B)*5 + (C and not(B))*6 + (not(B or C) and D)*7 + (not(B or D))*8
-        data[option // 3][option % 3] = r'$C_{\mathrm{mem}} \approx$' + f'{self.memory_usage:.3f}' + r', $C_{\mathrm{util}} \approx$' + f'{self.gpu_utilisation:.3f}'
-        
+        cell_colours = [['#0fa83480'], ['#ffc84480'], ['#e3555580']]
+
         plt.rcParams.update({'font.size': 18}) 
 
         table = plt.table(cellText = data,
@@ -40,10 +40,6 @@ class gpu_perf:
                           cellLoc = 'center',
                           rowLoc = 'center',
                           cellColours = cell_colours)
-        '''
-        for cell in table.get_celld().values():
-            cell.set_edgecolor('none')
-        '''
 
         table.scale(1.5,3)
         plt.show()
