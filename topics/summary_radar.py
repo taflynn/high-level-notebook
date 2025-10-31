687f221a347d148347772bc0b01aacd04e16b5a1
taflynn <thomas.a.flynn@durham.ac.uk>
2025-10-31 14:47:50 +0000

    updating the summary class to match the new interfaces

--- topics/summary_radar.py
+++ topics/summary_radar.py
@@ -1,34 +1,32 @@
 import pandas as pd
 import plotly.express as px
 class summary:
-    def __init__(self, core_perf, intra_node_perf):
-        ''' Placeholder for now '''
-    def draw_radar(self):
-        ''' Draw the Radar Diagram '''
-        ### TEMPORARY PLACEHOLDER VARIABLES ###
-        core_proportion = 0.82
-        intraNode_proportion = 0.9
-        interNode_proportion = 0.7
-        IO_proportion = 0.5
-        GPU_porporion = 0.3
+    def __init__(self, core_perf_stats, intra_node_perf_stats, inter_node_perf_stats, gpu_perf_stats, io_perf_stats):
+
+        self.core_perf_stats = core_perf_stats
+        self.intra_node_perf_stats = intra_node_perf_stats
+        self.inter_node_perf_stats = inter_node_perf_stats
+        self.gpu_perf_stats = gpu_perf_stats
+        self.io_perf_stats = io_perf_stats
+    
+    def summary_figure(self):
+        '''Generates a radar figure of the five performance metrics'''
         
-        # diagram variables
-        width = 1000
-        height = 700
-        
-        # define data to draw diagram
         data = pd.DataFrame(dict(
-            proportions = [core_proportion, intraNode_proportion, interNode_proportion, IO_proportion, GPU_porporion],
-            rubrics = ['Core','Intra node','Inter node',
-                   'I/O', 'GPU']))
+            proportions = [self.core_perf_stats.core_metric,
+                           self.intra_node_perf_stats.intra_node_prop_60,
+                           self.inter_node_perf_stats.inter_node_prop_60,
+                           self.gpu_perf_stats.gpu_metric,
+                           self.io_perf_stats.io_metric],
+            rubrics = ['Core','Intra node','Inter node', 'GPU', 'I/O']
+        ))
         
-        # declare diagram
-        fig = px.line_polar(data, r='proportions',
+        fig = px.line_polar(data, 
+                            r='proportions',
                             theta='rubrics',
                             line_close=True,
-                            range_r = (0,1),
-                            width = width, height = height)
+                            range_r = (0, 1),
+                            width = 1000, height = 700)
         
-        # draw diagram
         fig.update_traces(fill='toself')
         fig.show()
